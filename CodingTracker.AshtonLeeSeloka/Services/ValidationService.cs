7157a694eccd658f303aaa0dbd04cfba16d69203
ashtonseloka@outlook.com <ashtonseloka@outlook.com>
2024-12-28 12:07:41 +0200

    Started Data Validation

--- CodingTracker.AshtonLeeSeloka/Services/ValidationService.cs
+++ CodingTracker.AshtonLeeSeloka/Services/ValidationService.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Spectre.Console;
 
 namespace Services
 {
 	internal class ValidationService
 	{
+		public bool DateValidation(string message) 
+		{
+			while (true) 
+			{
+				try 
+				{
+
+					
+				}
+				catch 
+				{
+					break;
+				
+				}
+			
+			}
+
+			return false;
+		
+		}
 	}
 }
